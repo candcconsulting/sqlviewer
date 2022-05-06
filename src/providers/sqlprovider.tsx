7fd4c456fdaa5911fbf38a8cfb7802d730581738
candcconsulting <github@candcconsulting.co.uk>
2022-05-06 13:53:12 +0100

    Add hiliteall

--- src/providers/sqlprovider.tsx
+++ src/providers/sqlprovider.tsx
@@ -1,7 +1,10 @@
 import { AbstractWidgetProps, StagePanelLocation, StagePanelSection, UiItemsProvider } from "@itwin/appui-abstract";
 import { BrowserAuthorizationClient } from "@itwin/browser-authorization";
+import { ColorDef, FeatureAppearance } from "@itwin/core-common";
 import { EmphasizeElements, IModelApp, IModelConnection } from "@itwin/core-frontend";
 import { Blockquote, Button, Input, Textarea, Text } from "@itwin/itwinui-react";
+import { KeySet } from "@itwin/presentation-common";
+import { HiliteSetProvider } from "@itwin/presentation-frontend";
 import { AST, Parser } from "node-sql-parser";
 
 const _executeQuery = async (imodel: IModelConnection, query: string) => {
@@ -30,13 +33,28 @@ const SQLWidget = () => {
     }
   }
 
+  const getElementIdHiliteSet = async function(elementIds: string[], iModel: IModelConnection) {
+    if(iModel.isOpen) {
+      const provider = HiliteSetProvider.create({imodel: iModel});
+      const keys = elementIds.map(function(id: any){
+        return {id, className: "BisCore:GeometricElement"};
+      });
+      let keyset = new KeySet(keys);
+      const set = await provider.getHiliteSet(keyset);
+      if (set.elements === undefined)
+        return [];
+      return [...set.elements, ...elementIds];
+    }
+    return []
+  }
+
   async function handleExecuteSQL() : Promise<void> {
     const input = document.getElementById('sqlStatement') as HTMLInputElement;
     if (input) {
       const sql =  input.value
       const parser = new Parser();
       const ast = parser.astify(sql) as AST
-      if ((ast.type === "select") && ((ast.columns.indexOf("*") >= 0) || (ast.columns.indexOf("ecinstanceid") >= 0)) && (ast.where)) {
+      if ((ast.type === "select") && (ast.where)) {
         const vp = IModelApp.viewManager.getFirstOpenView();
         if (!vp) { return};
         const authClient = IModelApp.authorizationClient as BrowserAuthorizationClient
@@ -51,8 +69,11 @@ const SQLWidget = () => {
           const emph = EmphasizeElements.getOrCreate(vp);            
           emph.clearEmphasizedElements(vp);
           emph.clearOverriddenElements(vp);            
-          const ecResult = results.map(x => x[0]);            
-          emph.emphasizeElements(ecResult,vp, undefined, false);
+          const ecResult = results.map(x => x[0]);
+          const allElements = ecResult;
+          // const allElements = await getElementIdHiliteSet(ecResult, vp.iModel);
+          emph.emphasizeElements(allElements,vp, FeatureAppearance.fromRgb(ColorDef.from(127,0,0)), false);
+          vp.zoomToElements(allElements);
 
         }
       } else
